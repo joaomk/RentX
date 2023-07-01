5c09891df8b39d711996c18717ef814678c9a8b5
João Vitor da Silva <joaomk@live.com>
2023-07-01 01:45:10 -0300

    listCars and tests

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,4 +1,7 @@
+import { inject, injectable } from "tsyringe";
+
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
 interface IRequest {
@@ -6,9 +9,12 @@ interface IRequest {
   name?: string;
   brand?: string;
 }
-
-class ListCarsUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+@injectable()
+class ListAvailableCarsUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
 
   async execute({ name, brand, category_id }: IRequest): Promise<Car[]> {
     const cars = await this.carsRepository.findAvailable(
@@ -20,4 +26,4 @@ class ListCarsUseCase {
   }
 }
 
-export { ListCarsUseCase };
+export { ListAvailableCarsUseCase };
